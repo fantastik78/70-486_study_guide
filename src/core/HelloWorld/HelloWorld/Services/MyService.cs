f404361d5e86625eb6d5a43f361986d0c4997725
fanatstik78 <vtfantastik78@gmail.com>
2019-03-13 11:02:34 +0100

    Example of Service injection into a view

---
+++ src/core/HelloWorld/HelloWorld/Services/MyService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Services
+{
+    public class MyService : IMyService
+    {
+        public string SayHelloWorld()
+        {
+            return "Hello world from service";
+        }
+    }
+}
--- src/core/HelloWorld/HelloWorld/Startup.cs
+++ src/core/HelloWorld/HelloWorld/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HelloWorld.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -30,6 +31,7 @@ namespace HelloWorld
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddTransient<IMyService, MyService>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
